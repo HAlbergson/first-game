af74bf10979ed63ea06067b3d4f49d1592cab94a
HAlbergson <hjalte3480@gmail.com>
2023-03-02 12:59:12 +0100

    random position and speed added

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 "use strict";
 console.log("hallo world");
 window.addEventListener("load", start);
@@ -11,67 +9,108 @@ function start() {
   points = 0;
   lives = 3;
   console.log("noob");
+  startAnimation();
+  addPosition();
+  registerClick();
+  animationRestart();
+  addSpeed();
+}
+
+function startAnimation() {
   document.querySelector("#mouse1_container").classList.add("leftzigzag");
   document.querySelector("#mouse2_container").classList.add("leftzigzag");
   document.querySelector("#cat1_container").classList.add("leftzigzag");
   document.querySelector("#cat2_container").classList.add("leftzigzag");
-
+}
+function addPosition() {
+  document.querySelector("#mouse1_container").classList.add("position1");
+  document.querySelector("#mouse2_container").classList.add("position3");
+  document.querySelector("#cat1_container").classList.add("position2");
+  document.querySelector("#cat2_container").classList.add("position5");
+}
+function registerClick() {
   document.querySelector("#mouse1_container").addEventListener("mousedown", clickMouse);
   document.querySelector("#mouse2_container").addEventListener("mousedown", clickMouse);
   document.querySelector("#cat1_container").addEventListener("mousedown", clickCat);
   document.querySelector("#cat2_container").addEventListener("mousedown", clickCat);
 }
 
+function animationRestart() {
+  document.querySelector("#mouse1_container").addEventListener("animationiteration", mouseRestart);
+  document.querySelector("#mouse2_container").addEventListener("animationiteration", mouseRestart);
+  document.querySelector("#cat1_container").addEventListener("animationiteration", catRestart);
+  document.querySelector("#cat2_container").addEventListener("animationiteration", catRestart);
+}
+
+function addSpeed() {
+  document.querySelector("#mouse1_container").classList.add("speed1");
+  document.querySelector("#mouse2_container").classList.add("speed2");
+  document.querySelector("#cat1_container").classList.add("speed3");
+  document.querySelector("#cat2_container").classList.add("speed2");
+}
+
 function clickMouse() {
   let mouse = this;
-  mouse.removeEventListener("click", clickMouse);
-
+  mouse.removeEventListener("mousedown", clickMouse);
   mouse.classList.add("paused");
   mouse.querySelector("img").classList.add("zoom_out");
   mouse.addEventListener("animationend", mouseGone);
   incrementPoints();
 }
 
+function mouseGone() {
+  let mouse = this;
+  mouse.removeEventListener("animationend", mouseGone);
+  mouse.querySelector("img").classList.remove("zoom_out");
+  mouse.classList.remove("paused");
+  mouseRestart.call(this);
+  mouse.addEventListener("mousedown", clickMouse);
+}
+
+function mouseRestart() {
+  console.log("mouserESTART");
+  let mouse = this;
+  mouse.classList.remove("leftzigzag");
+  mouse.offsetWidth;
+  mouse.classList.add("leftzigzag");
+  mouse.classList.remove("speed1", "speed2", "speed3");
+  let speed = Math.floor(Math.random() * 3) + 1;
+  mouse.classList.add("speed" + speed);
+  mouse.classList.remove("position1", "position2", "position3", "position4", "position5");
+  let pos = Math.floor(Math.random() * 5) + 1;
+  mouse.classList.add("position" + pos);
+}
 
 function clickCat() {
   let cat = this;
   cat.removeEventListener("click", clickCat);
-
   cat.classList.add("paused");
   cat.querySelector("img").classList.add("zoom_out");
   cat.addEventListener("animationend", catGone);
   decrementLives();
 }
 
-
-function mouseGone() {
-  let mouse = this;
-
-  mouse.removeEventListener("animationend", mouseGone);
-  mouse.querySelector("img").classList.remove("zoom_out");
-
-  mouse.classList.remove("paused");
-
-  mouse.classList.remove("leftzigzag");
-  mouse.offsetWidth;
-  mouse.classList.add("leftzigzag");
-
-  mouse.addEventListener("click", clickMouse);
-}
-
 function catGone() {
   let cat = this;
   cat.removeEventListener("animationend", catGone);
-
   cat.querySelector("img").classList.remove("zoom_out");
-
   cat.classList.remove("paused");
+  catRestart.call(this);
+  cat.addEventListener("mousedown", clickCat);
+}
 
+function catRestart() {
+  console.log("catrESTART");
+  let cat = this;
   cat.classList.remove("leftzigzag");
   cat.offsetWidth;
   cat.classList.add("leftzigzag");
-
-  cat.addEventListener("click", clickCat);
+  cat.classList.remove("speed1", "speed2", "speed3");
+  let speed = Math.floor(Math.random() * 3) + 1;
+  cat.classList.add("speed" + speed);
+  cat.classList.remove("position1", "position2", "position3", "position4", "position5");
+  let pos = Math.floor(Math.random() * 5) + 1;
+  cat.classList.add("position" + pos);
 }
 
 function incrementPoints() {
