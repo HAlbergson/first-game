b3345c3b7c1f66d4a8b76b8f18d122de9c601aec
HAlbergson <hjalte3480@gmail.com>
2023-03-02 11:02:20 +0100

    optimeret js

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 "use strict";
 console.log("hallo world");
 window.addEventListener("load", start);
@@ -9,157 +11,67 @@ function start() {
   points = 0;
   lives = 3;
   console.log("noob");
-  document.querySelector("#mouse1_container").classList.add("rightzigzag");
+  document.querySelector("#mouse1_container").classList.add("leftzigzag");
   document.querySelector("#mouse2_container").classList.add("leftzigzag");
   document.querySelector("#cat1_container").classList.add("leftzigzag");
-  document.querySelector("#cat2_container").classList.add("rightzigzag");
+  document.querySelector("#cat2_container").classList.add("leftzigzag");
 
-  document
-    .querySelector("#mouse1_container")
-    .addEventListener("mousedown", clickMouse1);
-  document
-    .querySelector("#mouse2_container")
-    .addEventListener("mousedown", clickMouse2);
-  document
-    .querySelector("#cat1_container")
-    .addEventListener("mousedown", clickCat1);
-  document
-    .querySelector("#cat2_container")
-    .addEventListener("mousedown", clickCat2);
+  document.querySelector("#mouse1_container").addEventListener("mousedown", clickMouse);
+  document.querySelector("#mouse2_container").addEventListener("mousedown", clickMouse);
+  document.querySelector("#cat1_container").addEventListener("mousedown", clickCat);
+  document.querySelector("#cat2_container").addEventListener("mousedown", clickCat);
 }
 
-function clickMouse1() {
-  console.log("virker det");
-  document
-    .querySelector("#mouse1_container")
-    .removeEventListener("click", clickMouse1);
+function clickMouse() {
+  let mouse = this;
+  mouse.removeEventListener("click", clickMouse);
 
-  document.querySelector("#mouse1_container").classList.add("paused");
-  document.querySelector("#mouse1_sprite").classList.add("zoom_out");
-  document
-    .querySelector("#mouse1_container")
-    .addEventListener("animationend", mouse1Gone);
+  mouse.classList.add("paused");
+  mouse.querySelector("img").classList.add("zoom_out");
+  mouse.addEventListener("animationend", mouseGone);
   incrementPoints();
 }
 
-function clickMouse2() {
-  console.log("virker det2");
-  document
-    .querySelector("#mouse2_container")
-    .removeEventListener("click", clickMouse1);
 
-  document.querySelector("#mouse2_container").classList.add("paused");
-  document.querySelector("#mouse2_sprite").classList.add("zoom_out");
-  document
-    .querySelector("#mouse2_container")
-    .addEventListener("animationend", mouse2Gone);
-  incrementPoints();
-}
+function clickCat() {
+  let cat = this;
+  cat.removeEventListener("click", clickCat);
 
-function clickCat1() {
-  console.log("virker det3");
-  document
-    .querySelector("#cat1_container")
-    .removeEventListener("click", clickCat1);
-
-  document.querySelector("#cat1_container").classList.add("paused");
-  document.querySelector("#cat1_sprite").classList.add("zoom_out");
-  document
-    .querySelector("#cat1_container")
-    .addEventListener("animationend", cat1Gone);
-  decrementLives();
-}
-function clickCat2() {
-  console.log("virker det3");
-  document
-    .querySelector("#cat2_container")
-    .removeEventListener("click", clickCat2);
-
-  document.querySelector("#cat2_container").classList.add("paused");
-  document.querySelector("#cat2_sprite").classList.add("zoom_out");
-  document
-    .querySelector("#cat2_container")
-    .addEventListener("animationend", cat2Gone);
+  cat.classList.add("paused");
+  cat.querySelector("img").classList.add("zoom_out");
+  cat.addEventListener("animationend", catGone);
   decrementLives();
 }
 
-function mouse1Gone() {
 
-  document
-    .querySelector("#mouse1_container")
-    .removeEventListener("animationend", mouse1Gone);
+function mouseGone() {
+  let mouse = this;
 
+  mouse.removeEventListener("animationend", mouseGone);
+  mouse.querySelector("img").classList.remove("zoom_out");
 
-  document.querySelector("#mouse1_sprite").classList.remove("zoom_out");
+  mouse.classList.remove("paused");
 
+  mouse.classList.remove("leftzigzag");
+  mouse.offsetWidth;
+  mouse.classList.add("leftzigzag");
 
-  document.querySelector("#mouse1_container").classList.remove("paused");
-
-
-  document.querySelector("#mouse1_container").classList.remove("rightzigzag");
-  document.querySelector("#mouse1_container").offsetWidth;
-  document.querySelector("#mouse1_container").classList.add("rightzigzag");
-
-
-  document
-    .querySelector("#mouse1_container")
-    .addEventListener("click", clickMouse1);
+  mouse.addEventListener("click", clickMouse);
 }
 
-function mouse2Gone() {
-  document
-    .querySelector("#mouse2_container")
-    .removeEventListener("animationend", mouse2Gone);
+function catGone() {
+  let cat = this;
+  cat.removeEventListener("animationend", catGone);
 
-  document.querySelector("#mouse2_sprite").classList.remove("zoom_out");
+  cat.querySelector("img").classList.remove("zoom_out");
 
-  document.querySelector("#mouse2_container").classList.remove("paused");
+  cat.classList.remove("paused");
 
-  document.querySelector("#mouse2_container").classList.remove("leftzigzag");
-  document.querySelector("#mouse2_container").offsetWidth;
-  document.querySelector("#mouse2_container").classList.add("leftzigzag");
+  cat.classList.remove("leftzigzag");
+  cat.offsetWidth;
+  cat.classList.add("leftzigzag");
 
-  document
-    .querySelector("#mouse2_container")
-    .addEventListener("click", clickMouse2);
-}
-
-function cat1Gone() {
-  document
-    .querySelector("#cat1_container")
-    .removeEventListener("animationend", cat1Gone);
-
-  document.querySelector("#cat1_sprite").classList.remove("zoom_out");
-
-  document.querySelector("#cat1_container").classList.remove("paused");
-
-  document.querySelector("#cat1_container").classList.remove("leftzigzag");
-  document.querySelector("#cat1_container").offsetWidth;
-  document.querySelector("#cat1_container").classList.add("leftzigzag");
-
-  document
-    .querySelector("#cat1_container")
-    .addEventListener("click", clickCat1);
-}
-
-function cat2Gone() {
-
-  document
-    .querySelector("#cat2_container")
-    .removeEventListener("animationend", cat2Gone);
-
-
-  document.querySelector("#cat2_sprite").classList.remove("zoom_out");
-
-  document.querySelector("#cat2_container").classList.remove("paused");
-
-  document.querySelector("#cat2_container").classList.remove("rightzigzag");
-  document.querySelector("#cat2_container").offsetWidth;
-  document.querySelector("#cat2_container").classList.add("rightzigzag");
-
-  document
-    .querySelector("#cat2_container")
-    .addEventListener("click", clickCat2);
+  cat.addEventListener("click", clickCat);
 }
 
 function incrementPoints() {
